e47b2123ddd3d32675bd1ac23d11fd1833f23d4b
Marcin Wrembel <marcin.wrembel@interia.pl>
2023-04-15 19:50:10 +0200

    upd

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     static: {
-        directory: path.join(__dirname, 'dist'),
+        directory: path.join(__dirname, 'public'),
       },
     open: true,
     watchFiles: ['src/index.html'],
